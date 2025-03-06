2557769cdbf3f74fb8df944d8ff3e3d12fa7eb32
Olly Costello <ollycostello@hotmail.co.uk>
2025-03-06 22:43:49 +0000

    Add browser icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import MortgageCalculator from './components/MortgageCalculator';
 
+const favicon = document.getElementById('browsericon');
+if (process.env.NODE_ENV === 'development') {
+  favicon.href = `${process.env.PUBLIC_URL}/react.ico`;
+} else {
+  favicon.href = `${process.env.PUBLIC_URL}/mm-icon.ico`;
+}
+
 function App() {
   return (
     <div className="App">
